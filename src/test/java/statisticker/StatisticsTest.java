3d6d66efbbb631098076036e096901a825782fbc
Rakeshmallapur <105627827+Rakeshmallapur@users.noreply.github.com>
2022-05-23 14:10:53 +0530

    Update StatisticsTest.java

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -15,39 +15,38 @@ public class StatisticsTest
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
-        Statistics.Stats s = Statistics.getStatistics(numberList);
+        Stats s = Statistics.getStatistics(numberList);
 
         float epsilon = 0.001f;
-        assertEquals(s.average, 4.525f, epsilon);
-        assertEquals(s.min, 1.5f, epsilon);
-        assertEquals(s.max, 8.9f, epsilon);
+        assertEquals(s.getAverage(), 4.525f, epsilon);
+        assertEquals(s.getMin(), 1.5f, epsilon);
+       assertEquals(s.getMax(), 8.9f, epsilon);
     }
     @Test
     public void reportsNaNForEmptyInput()
     {
-        List<___> emptyList = new ArrayList<___>();
+        List<Float> emptyList = new ArrayList<Float>();
 
-        Statistics.Stats s = Statistics.getStatistics(emptyList);
+        Stats s = Statistics.getStatistics(emptyList);
+        float epsilon = 0.001f;
+        assertEquals(s.getAverage(), Float.NaN, epsilon);
+        assertEquals(s.getMax(), Float.NaN, epsilon);
+        assertEquals(s.getMin(), Float.NaN, epsilon);
 
-        //All fields of computedStats (average, max, min) must be
-        //Float.NaN (not-a-number), as described in
-        //https://www.geeksforgeeks.org/nan-not-number-java/
-        //Design the asserts here and implement accordingly.
     }
     @Test
     public void reportsAlertsIfMaxIsMoreThanThreshold()
     {
         EmailAlert emailAlerter = new EmailAlert();
         LEDAlert ledAlerter = new LEDAlert();
-        IAlerter alerters[] = {emailAlerter, ledAlerter};
         float maxThreshold = 10.2f;
-        StatsChecker checker = new StatsChecker(maxThreshold, alerters);
+        StatsChecker checker = new StatsChecker(maxThreshold, emailAlerter , ledAlerter);
 
         Float[] numbers = {11.5f, 6.9f, 7.5f, 6.6f};
-        List<___> numberList = Arrays.asList(numbers);
-        checker.checkAndAlert(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
+        checker.checkAndAlert(numberList);
         
         assertTrue(emailAlerter.emailSent);
         assertTrue(ledAlerter.ledGlows);
